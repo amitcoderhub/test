d31a5fea0cb83978ad9440f157d60c2d0af6d75d
amitcoderhub <amitchauhan6599@gmail.com>
2025-03-18 22:18:24 +0530

    u

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ app.get("/api", (req, res) => {
   res.json({ message: "Backend is deployed successfully!" });
 });
 
+// Serve frontend index.html for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
